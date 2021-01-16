f35387ba1309395b02d0640357e7fec225936072
Andey Galchevski <andreygalchevski@gmail.com>
2021-01-16 18:48:02 +0200

    revert to using google fonts instead of self-hosting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,7 +39,11 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link href="/fonts/style.css" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
